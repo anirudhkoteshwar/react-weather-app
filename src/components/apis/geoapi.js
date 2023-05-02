364a78564736aaac63843fe1e8edb21e6c4bdc56
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2023-05-02 17:33:18 +0530

    weather api works

---
+++ src/components/apis/geoapi.js
@@ -0,0 +1,8 @@
+export const Geo_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities";
+export const GeoAPIOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "ec7bbde42emsh8a5c40c168ca83cp10953djsncf6cd944212c",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
