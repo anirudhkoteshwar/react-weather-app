c3231641a7e828e69847c704594a486456fafa7c
anirudhkoteshwar <anirudh.koteshwar@gmail.com>
2023-05-02 19:42:23 +0530

    weather api works

--- src/components/apis/openweather.js
+++ src/components/apis/openweather.js
@@ -1,2 +1,2 @@
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
-export const WEATHER_API_KEY = "e5f246815cdafe988ee50a613dc112a9"
+export const WEATHER_API_KEY = process.env.REACT_APP_WEATHER_KEY
